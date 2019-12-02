e733675cddd6eca1b26db6ce1b9a4ec01756e691
Cartier1288 <cartier1288@gmail.com>
2019-12-02 18:48:49 -0500

    Removed square move function

--- biquadris2/Biquadris/src/square.cc
+++ biquadris2/Biquadris/src/square.cc
@@ -33,11 +33,6 @@ bool Square::move(Direction direction, unsigned int shift)
 	return validPosition();
 }
 
-bool Square::move(Coordinate newPosition)
-{
-	return validPosition();
-}
-
 void Square::kill()
 {
 	this->status = SquareStatus::DEAD;
