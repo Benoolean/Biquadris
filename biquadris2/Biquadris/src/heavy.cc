0759451156f46c9696fa9bd7179a97cf5db9b932
Cartier1288 <cartier1288@gmail.com>
2019-12-03 01:24:08 -0500

    made heavy effect work

--- biquadris2/Biquadris/src/heavy.cc
+++ biquadris2/Biquadris/src/heavy.cc
@@ -1,3 +1,16 @@
 #include "../headers/heavy.h"
+using namespace Biquadris;
 
 Heavy::Heavy(Grid* component) : Effect(component) { }
+
+bool Heavy::move(Direction direction) {
+  bool moved = component->move(direction);
+
+  //Shift down twice if horizontal movement
+  if(direction != Direction::DOWN && moved) {
+    moved = component->move(Direction::DOWN);
+    if(moved) moved = component->move(Direction::DOWN);
+  }
+
+  return moved;
+}
